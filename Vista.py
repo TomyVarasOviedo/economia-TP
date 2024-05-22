cf4d5768d4635a58420a148a2070900c25c84b7e
TomyVarasOviedo <tomasvarasoviedo@gmail.com>
2024-05-22 20:26:12 -0300

    Valor Final: Mitad implementado

--- Vista.py
+++ Vista.py
@@ -9,20 +9,24 @@ ctk.set_default_color_theme("dark-blue")
 # Crear ventana principal
 root = ctk.CTk()
 root.title("Rentas")
-root.geometry("600x400")
+root.geometry("700x450")
 def run():
     root.mainloop()
 
-def capturarDatos():
-    cuotas = cuotas_entry.get()
-    tasa_interes = tasa_entry.get()
+def verValorFinal():
+    cuotas = int(cuotas_entry.get())
+    tasa_interes = float(tasa_entry.get())
     periodo_inicial = periodo_inicial_cal.get_date()
     periodo_final = periodo_final_cal.get_date()
     piy,pim,pid = map(int, str(periodo_inicial).split("-"))
     pfy, pfm,pfd = map(int, str(periodo_final).split("-"))
-    
-    print(Renta.calcularValor(float(tasa_interes), int(cuotas),date(piy,pim,pid), date(pfy,pfm,pfd)))
 
+    if detalle.winfo_ismapped():
+        detalle.place_forget()
+    else:
+        detalle.pack()
+    valor_final_label.configure(text=f"Valor Final: {Renta.calcularValor(tasa_interes, cuotas, date(piy,pim,pid), date(pfy,pfm,pfd)):.02f}")
+        
 # Frame principal
 frame = ctk.CTkFrame(master=root)
 frame.pack(padx=20, pady=20,)
@@ -53,10 +57,16 @@ periodo_final_cal = DateEntry(frame, selectmode='day',
 periodo_final_cal.grid(row=3, column=1, padx=10, pady=5)
 
 # Botón para enviar datos
-enviar_button = ctk.CTkButton(master=frame, text="Calcular", font=("Roboto", 16),command=capturarDatos)
-enviar_button.grid(row=5, column=1, columnspan=3, pady=10, padx=8)
+enviar_button = ctk.CTkButton(master=frame, text="Calcular", font=("Roboto", 16),command=verValorFinal)
+enviar_button.grid(row=5, column=1, pady=10, padx=8)
 
 # Botón con icono
 # Agregar un botón con el carácter "<-"
 boton_icono = ctk.CTkButton(master=frame, text="↔",width=3)
 boton_icono.grid(row=5, column=0, pady=5)
+
+# Ver detalle 
+detalle = ctk.CTkFrame(master=root)
+valor_final_label = ctk.CTkLabel(master=detalle, text="", font=("Roboto", 16))
+valor_final_label.grid(row=0,column=0, padx=10,pady=20, sticky="w")
+detalle.place_forget()
